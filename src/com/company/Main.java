6f0c4dd192231b6fbb30dd6cbc0478c5d0093a58
gulshoda10 <gulichka_rd@yahoo.com>
2021-07-25 14:06:49 -0700

    Trigger

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-	   for(int i=0; i<7; i++){
+	   for(int i=0; i<8; i++){
            System.out.println("Batch 9 will get 150K salary jobs");
            Thread.sleep(1000);
        }
