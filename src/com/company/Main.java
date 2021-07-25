cdf8b188d06faedb5384fbfbe3da646b709b126a
gulshoda10 <gulichka_rd@yahoo.com>
2021-07-25 13:01:10 -0700

    New commits

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	   for(int i=0; i<7; i++){
+           System.out.println("Batch 9 will get 150K salary jobs");
+           Thread.sleep(1000);
+       }
+    }
+}
